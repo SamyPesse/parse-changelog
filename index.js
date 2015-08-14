4d3209fc919c8622db59e0d87aeb85a7bd25508d
Samy Pessé <samypesse@gmail.com>
2015-08-14 12:46:00 +0200

    Extract versions tags

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ var kramed = require('kramed');
 
 
 
-module.exports = function(source) {
+module.exports = function(source, opts) {
+    opts = _.defaults(opts || {}, {
+        title: true
+    });
+
+
+    var version = null;
+    var titleDepth = 0;
     var changelog = {
         title: "",
         intro: "",
@@ -13,8 +20,27 @@ module.exports = function(source) {
     var lexer = new kramed.Lexer({});
 
     var tokens = lexer.lex(source);
+    _.each(tokens, function(token) {
+        // First heading is title
+        if (opts.title && !changelog.title && token.type == 'heading') {
+            titleDepth = token.depth;
+            changelog.title = token.text;
+        }
 
-    if (tokens[0].type == 'heading') changelog.title = tokens[0].text;
+        // First paragraph is intro
+        else if (!version && !changelog.intro && token.type == 'paragraph') {
+            changelog.intro = token.text;
+        }
+
+        else if (token.type == 'heading' && token.depth > titleDepth) {
+            if (version) changelog.versions.push(version);
+            version = {
+                tag: token.text
+            };
+        }
+    });
+
+    if (version) changelog.versions.push(version);
 
     return changelog;
 };
--- test/changelog.js
+++ test/changelog.js
@@ -12,4 +12,13 @@ describe('ChangeLog', function() {
         changelog.title.should.be.exactly('Release notes');
     })
 
+    it('should correctly extract intro', function() {
+        changelog.should.have.property('intro');
+        changelog.intro.should.be.exactly('All notable changes to this project will be documented in this file.\nThis project adheres to [Semantic Versioning](http://semver.org/).');
+    })
+
+    it('should correctly extract all versions', function() {
+        changelog.should.have.property('versions');
+        changelog.versions.should.have.lengthOf(41);
+    })
 });
